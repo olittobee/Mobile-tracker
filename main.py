b9de7c212d7b19884d6605ea50d9ae0fd4f9f7c0
Elijah Bayere Olaiya <99737422+olittobee@users.noreply.github.com>
2023-03-23 01:46:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import phonenumbers
+from phonenumbers import carrier , geocoder , timezone
+
+mobileNo = input("ENTER MOBILE NUMBER WITH COUNTRY CODE: ")
+mobileNo = phonenumbers.parse(mobileNo)
+
+#get timezone a phone number 
+print(timezone.time_zones_for_number(mobileNo))
+
+#getting a carrier for phonenumbers
+print(carrier.name_for_number(mobileNo,"en"))
+
+#getting region information
+print(geocoder.description_for_number(mobileNo, "en"))
+
+#validating a phonenumber
+print("VALID MOBILE NUMBER: ", phonenumbers.is_valid_number(mobileNo))
+
+#checking possibility of number
+print("Checking possibility of number{}...", phonenumbers.is_possible_number(mobileNo))
+# made by @GRINDis37
